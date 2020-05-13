b36e1df4d279c69d80ab7434973c19d479890fd0
xiaoyun-matroid <xiaoyun@matroid.com>
2020-05-13 15:43:52 -0700

    Update videos

--- test/test-videos.js
+++ test/test-videos.js
@@ -3,28 +3,28 @@ const expect = chai.expect;
 const { setUpClient } = require('./utils');
 const { EVERYDAY_OBJECT_ID, YOUTUBE_VID_URL } = require('./data');
 
-describe('Videos', function() {
+describe('Videos', function () {
   this.timeout(10000);
 
   let videoId;
 
-  before(async function() {
+  before(async function () {
     this.api = setUpClient();
     await this.api.retrieveToken();
   });
 
-  describe('classifyVideo', async function() {
-    it('should start classifying a video', async function() {
+  describe('classifyVideo', async function () {
+    it('should start classifying a video', async function () {
       const res = await this.api.classifyVideo(EVERYDAY_OBJECT_ID, {
-        url: YOUTUBE_VID_URL
+        url: YOUTUBE_VID_URL,
       });
 
-      expect(res.video_id).to.be.a('string', JSON.stringify(res));
+      expect(res.videoId).to.be.a('string', JSON.stringify(res));
 
-      videoId = res.video_id;
+      videoId = res.videoId;
     });
 
-    it('should throw and error if missing video', async function() {
+    it('should throw and error if missing video', async function () {
       try {
         await this.api.classifyVideo(EVERYDAY_OBJECT_ID);
       } catch (e) {
@@ -37,15 +37,17 @@ describe('Videos', function() {
     });
   });
 
-  describe('getVideoResults', async function() {
-    it('should get video classification results', async function() {
-      const res = await this.api.getVideoResults(videoId);
+  describe('getVideoResults', async function () {
+    it('should get video classification results', async function () {
+      const res = await this.api.getVideoResults(videoId, {
+        annotations: true,
+      });
 
-      expect(res).to.have.property('classification_progress');
+      expect(res).to.have.property('classificationProgress');
       expect(res).to.have.property('detections');
     });
 
-    it('should throw an error if missing videoId', async function() {
+    it('should throw an error if missing videoId', async function () {
       try {
         await this.api.getVideoResults();
       } catch (e) {
